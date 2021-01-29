ff013e33f9741a3768f8865da3a5a6da48592385
thangaramanujan <77881820+thangaramanujan@users.noreply.github.com>
2021-01-29 20:53:34 +0530

    -Dog class extends animal -Fish class extends animal

---
+++ src/Study/programming/Main.java
@@ -0,0 +1,15 @@
+package Study.programming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal", 1,1,5,5);
+
+        Dog dog = new Dog("YorkiE",8,20,2,4,1,20,"long silky");
+        Fish fish = new Fish("Glorie",10,10,1,2,2);
+     //   dog.eat();
+        dog.walk();
+     //   dog.run();
+
+    }
+}
